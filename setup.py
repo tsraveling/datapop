4ce749dbf7e9842a995c1f2f066041f06c88652b
tsraveling <tsraveling@gmail.com>
2017-03-26 22:43:31 -0500

    Updated project structure

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="myhello",
-    version='0.1',
-    py_modules=['hello'],
+    version='0.0.1',
+    name="datapop",
+    description="A data population and encryption tool for game makers and other content creators.",
+    author="Tim Raveling",
+    author_email="tsraveling@gmail.com"
+    py_modules=['datapop'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        myhello=hello:cli
+        datapop=datapop:cli
     ''',
 )
